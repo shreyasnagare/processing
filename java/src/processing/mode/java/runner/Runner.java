eba97f7351a6e29a6992fbf298aa4bcc9f0a473c
Shreyas Nagare <shreyasnagare@users.noreply.github.com>
2017-12-30 22:14:21 +0530

    Update Runner.java

--- java/src/processing/mode/java/runner/Runner.java
+++ java/src/processing/mode/java/runner/Runner.java
@@ -132,10 +132,7 @@ public class Runner implements MessageConsumer {
     try {
       InetAddress address = InetAddress.getByName("localhost");
       if (!address.getHostAddress().equals("127.0.0.1")) {
-        System.err.println("Your computer is not properly mapping 'localhost' to '127.0.0.1',");
-        System.err.println("which prevents sketches from working properly because 'localhost'");
-        System.err.println("is needed to connect the PDE to your sketch while it's running.");
-        System.err.println("If you don't recall making this change, or know how to fix it:");
+        System.err.println("Your computer is not properly mapping 'localhost' to '127.0.0.1',\nwhich prevents sketches from working properly because 'localhost' \nis needed to connect the PDE to your sketch while it's running.\nIf you don't recall making this change, or know how to fix it:");
         System.err.println("https://www.google.com/search?q=add+localhost+to+hosts+file+" + Platform.getName());
         throw new SketchException("Cannot run due to changes in your 'hosts' file. " +
                                   "See the console for details.", false);
